f6199b5283ac919ef94baad0c9c023722cbb0dc0
sebasc86 <sebascoscia@gmail.com>
2018-11-12 17:23:44 -0300

    validacion de campo

--- app/Http/Controllers/newTicketController.php
+++ app/Http/Controllers/newTicketController.php
@@ -3,30 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Ticket;
 
 class newTicketController extends Controller
 {
      public function index()
     {
-        return view('newTicket');
+    	
+     
+
+     /* $pastel = new Ticket;
+
+	$pastel->status = 1;
+	$pastel->apartament  = 'chessecake';
+	$pastel->number = 444444 ;
+	$pastel->save();
+*/
+	 return view('newTicket');
     }
 
     public function store()
     {
         
-        $this->validate(request(), [       
-          'name' => 'required|numeric',
-          /*'email' => 'unique:users,email,'.$user->id,
+      $this->validate(request(), [       
+/*          'name' => 'required|numeric',
+          'email' => 'unique:users,email,'.$user->id,
           'password' => 'required|alpha_num|min:8|max:12',
           'password_confirmation' => 'required|same:password',
           'accion' => 'required|array',
           'accion.driver' =>  'min:1|max:1',
           'accion.co-driver' =>  'min:2|max:2',
           'profile_picture' => 'max:2048|mimes:jpg,jpeg,gif,png',*/
-          ]);
+          'email' => 'required|email',
+      ]);
+	
+
+      $tickets =  Ticket::all()->last();
+      if($tickets) {
+      	$ticketLastId = $tickets->id;
+      } else {
+      	$ticketLastId = 0;
+      }
+      
+
+      $ticket = new Ticket;
+
+	$ticket->status = 1;
+	$ticket->apartament  = 'chessecake';
+	$ticket->client = request()->clientN;
+	$ticket->number = date('Ymd') . 0 . $ticketLastId + 1;
+	$ticket->save();
+	
+          
+       /*$user->document = request()->document;
+	    $user->phone = request()->phone;
+	    $user->update();
+	   	Car::create([
+	    	'user_id' => Auth::user()->id,
+	    	'trademark' => request('trademark'),
+	    	'model' => request('model'),
+	    	'year' => request('year'),
+	    	'color' => request('color'),
+	    	'license_plate' => request('license_plate'),
+	    	'capacity' => request('capacity'),
+	    ]);    */
+
+
+          
+        
         return redirect('newTicket');   
 
-        
+
           
     }
 }
---
+++ database/migrations/2018_11_12_150631_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('status');
+            $table->string('apartament');
+            $table->bigInteger('number');
+            $table->integer('client')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
