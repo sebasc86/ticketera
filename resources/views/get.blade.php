c33383c8dbe24e43da8c340646e30fdbc1ad79d4
Sebastian <sebascoscia@gmail.com>
2018-11-24 03:11:24 -0300

    cambios datatables balde get sent

--- resources/views/get.blade.php
+++ resources/views/get.blade.php
@@ -1,38 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-	 <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>TicketCall</title>
-	<head>
-   		 <!-- Required meta tags -->
-   		 <meta charset="utf-8">
-   		 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.master')
 
-  		  <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">	
-        
-        
-       {{-- agregado el datatables --}}
-       <script type="text/javascript" src='https://code.jquery.com/jquery-3.3.1.js'></script>
-       <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-       <script src="https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js"></script>
-       
- 
-       <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.4/b-colvis-1.5.4/b-flash-1.5.4/b-html5-1.5.4/b-print-1.5.4/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.css"/>
-       
-       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-       <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.4/b-colvis-1.5.4/b-flash-1.5.4/b-html5-1.5.4/b-print-1.5.4/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.js"></script>
-       
-                
-  	</head>
-<body>
-
-  @include('header')
-
-
+@section('content')
+    
   <div class="container mt-5">
       <table class='table' id='tickets-table'>
       <thead >
@@ -64,8 +34,17 @@
       </tfoot>
       </table>
   </div>
+@endsection
 
- <script>
+@push('scripts')
+
+<script type="text/javascript" src='https://code.jquery.com/jquery-3.3.1.js'></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.4/b-colvis-1.5.4/b-flash-1.5.4/b-html5-1.5.4/b-print-1.5.4/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.js"></script>
+<script>
  
 $(document).ready(function() {
   var tickets = $('#tickets-table').DataTable({
@@ -110,15 +89,15 @@ $(document).ready(function() {
             { data: 'updated_at', name: 'updated_at', visible: false  },
             { data: 'number', 
               render: function(data){
-                return "<a href='http://127.0.0.1:8000/view/"+ data + "' class='view btn btn-primary'>Ver Ticket</button>"
+                return "<a href='http://127.0.0.1:8000/view/"+ data + "' class='view btn btn-dark'>Ver Ticket</button>"
               }
             },
         ],
     });
 });
  
- </script>
-
+</script>
+@endpush
 
 </body>
 </html>  
