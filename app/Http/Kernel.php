5b13d6a1ef73dc0a76eb8582ac4067f9c214005f
sebasc86 <sebascoscia@gmail.com>
2018-12-03 13:21:08 -0300

    cambio estilos manejo de errores sql agregado en handler/render

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'isAdmin' => \App\Http\Middleware\isAdminMiddleware::class,
+        'admin' => \App\Http\Middleware\isAdminMiddleware::class,
     ];
 
     /**
--- app/Http/Middleware/isAdminMiddleware.php
+++ app/Http/Middleware/isAdminMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class isAdminMiddleware
 {
@@ -13,14 +14,14 @@ class isAdminMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $isAdmin)
+    public function handle($request, Closure $next)
     {   
-        if ($request->user()->isAdmin != 1) {
-            return redirect('home');
-         }
+        if (Auth::user() &&  Auth::user()->isAdmin == 1) {
+            return $next($request);
+        }
 
         
-        return $next($request);
+        return abort(403, 'No puede ingresar a esta pagina');
 
     }
 }
