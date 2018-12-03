de7ccce0f0e92fd2a8d5f9e97be35d638ba53331
Sebastian <sebascoscia@gmail.com>
2018-12-03 20:39:02 -0300

    js new falta continuar validacion

--- public/js/new.js
+++ public/js/new.js
@@ -1,7 +1,26 @@
 $( document ).ready(function() {
-    $('#submit').on('click', function(e){
-        $('.fondo_modal').css('display', 'flex');       
-    })
+    console.log('init.app')
+
+    $('#submit').on('click', validateSelect)
+
+    var errorLabel = ''
+
+   
+    function validateSelect() {
+        var inputSelectNode = $( "#formTarget option:selected" )
+        inputSelectNodeVal = inputSelectNode.val()
+        inputSelectNodeVal = parseInt(inputSelectNodeVal)    
+        if((inputSelectNodeVal) != NaN){
+            errorLabel = 'No es un numero'
+            console.log(errorLabel)
+        }
+    }
+    
+    console.log(errorLabel)
+
+    // $('#submit').on('click', function(e){
+    //     $('.fondo_modal').css('display', 'flex');       
+    // })
 });
 
 
