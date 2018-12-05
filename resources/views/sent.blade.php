176e3c44a437861076fcffb9e52c5874c901a19d
sebasc86 <sebascoscia@gmail.com>
2018-12-05 16:37:47 -0300

    cambios rutas mal puestas

--- resources/views/sent.blade.php
+++ resources/views/sent.blade.php
@@ -44,7 +44,7 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.18/b-1.5.4/b-colvis-1.5.4/b-flash-1.5.4/b-html5-1.5.4/b-print-1.5.4/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.js"></script> --}}
 
-<link rel="stylesheet" type="text/css" href="{{ asset('js/datatables.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/datatables.min.css') }}">
 <script src="{{ asset('js/datatables.min.js') }}"></script>
 
 <script>
