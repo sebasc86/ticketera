b09499911ccc909dd13da148f37dca55cb1488c3
sebasc86 <sebascoscia@gmail.com>
2018-12-12 15:48:06 -0300

    prueba

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Sector;
 
 class IndexController extends Controller
 {
@@ -12,10 +13,13 @@ class IndexController extends Controller
 
 
     		$user = Auth::user();
+    		$sector = Sector::All();
+    		
     		if(isset($user)){
     			$sectorAdmin = $user->sector->isAdmin;
     			return view('index')
-        	->with('sectorAdmin', $sectorAdmin);
+        	->with('sectorAdmin', $sectorAdmin)
+        	->with('sector', $sector);
     		}
     		
     		return view('index');
