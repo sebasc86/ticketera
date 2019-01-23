cbee462dd01fc17d5e150514a65de11a598a07e0
Sebastian <sebascoscia@gmail.com>
2019-01-23 18:05:21 -0300

    Jquery agregado

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,10 +9,15 @@
 				<!-- Required meta tags -->
 				<meta charset="utf-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                <meta name="csrf-token" content="{{ csrf_token() }}" />
-								
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+				
+				<!-- include libraries(jQuery, bootstrap) -->
+				<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
+
 				<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 				<script src="{{ asset('js/app.js') }}"></script>
+
+			
 
 				{{-- agregado el datatables --}}
 				<link rel="stylesheet" type="text/css" href="{{asset( 'css/index.css' )}}">
