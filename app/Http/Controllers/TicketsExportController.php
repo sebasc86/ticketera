45335c058723d0545bb660046847bf8d00fd2f55
Sebastian <sebascoscia@gmail.com>
2019-02-21 13:19:34 -0300

    modificado middelware tcl a SectorAdmin

--- app/Http/Controllers/TicketsExportController.php
+++ app/Http/Controllers/TicketsExportController.php
@@ -13,7 +13,7 @@ class TicketsExportController extends Controller
     public function __construct()
     {
                 $this->middleware('auth');
-                $this->middleware('tlc');
+                $this->middleware('sectorAdmin');
     }
 
 
--- app/Http/Middleware/isSectorAdminMiddleware.php
+++ app/Http/Middleware/isSectorAdminMiddleware.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Support\Facades\Auth;
 use App\Sector;
 
-class isTlcMiddleware
+class isSectorAdminMiddleware
 {
     /**
      * Handle an incoming request.
